9b05f881da593f2c7ddee1956febfac7559a502f
min33sky <min33sky@naver.com>
2022-09-19 18:18:14 +0900

    refactor: header logo svg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,19 @@
 import useOpenAlertDialog from '@/hooks/useOpenAlertDialog';
 import Link from 'next/link';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
+import TmbgLogo from './vectors/Logo';
 
 export default function Header() {
   const openDialog = useOpenAlertDialog();
-  const router = useRouter();
 
   return (
     <header className="mx-auto flex w-full max-w-4xl items-center justify-between bg-slate-700 py-5 px-2 text-white">
-      <Image
-        aria-label="link to home page"
-        src={'/logo.svg'}
-        alt="Logo"
-        width={100}
-        height={40}
-        className="cursor-pointer"
-        onClick={() => router.push('/')}
-      />
+      <Link href={'/'}>
+        <a aria-label="logo to go home">
+          <TmbgLogo className="text-slate-200" />
+        </a>
+      </Link>
 
-      <div className="flex items-center gap-x-2 text-tahiti-600">
+      <div className="flex items-center gap-x-2 text-slate-200">
         <Link href={'/posts'}>
           <a>Posts</a>
         </Link>
