eaa5bc42499c96701a3d6648c28f9baa6e178fc3
min33sky <min33sky@naver.com>
2022-09-27 18:00:47 +0900

    refactor: header logo change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,26 @@
 import useOpenAlertDialog from '@/hooks/useOpenAlertDialog';
 import Link from 'next/link';
-import TmbgLogo from './vectors/Logo';
+import Logo from './vectors/Logo';
 
 export default function Header() {
   const openDialog = useOpenAlertDialog();
 
   return (
-    <header className="mx-auto flex w-full max-w-4xl items-center justify-between bg-slate-700 py-5 px-2 text-white">
-      <Link href={'/'}>
-        <a aria-label="logo to go home">
-          <TmbgLogo className="text-slate-200" />
-        </a>
-      </Link>
-
-      <div className="flex items-center gap-x-2 text-slate-200">
-        <Link href={'/posts'}>
-          <a>Posts</a>
+    <header className="h-20 w-full bg-slate-700 text-white">
+      <div className="mx-auto flex max-w-5xl items-center justify-between bg-slate-700 py-5 px-3 text-white">
+        <Link href={'/'}>
+          <a aria-label="logo to go home">
+            <Logo className="text-white" />
+          </a>
         </Link>
-        <div onClick={() => openDialog('not-ready')}>Menu1</div>
-        <div onClick={() => openDialog('not-ready')}>Menu2</div>
+
+        <div className="flex items-center gap-x-2 text-slate-200">
+          <Link href={'/posts'}>
+            <a>Posts</a>
+          </Link>
+          <div onClick={() => openDialog('not-ready')}>Menu1</div>
+          <div onClick={() => openDialog('not-ready')}>Menu2</div>
+        </div>
       </div>
     </header>
   );
